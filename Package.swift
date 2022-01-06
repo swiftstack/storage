6d664a63cfcfd27263c129753343710d5a34210a
Tony Freeman <tonyfreeman@users.noreply.github.com>
2022-01-06 08:53:15 +0300

    [Package] Add minimum target requirements

--- Package.swift
+++ Package.swift
@@ -3,6 +3,10 @@ import PackageDescription
 
 let package = Package(
     name: "Storage",
+    platforms: [
+        .iOS(.v13),
+        .macOS(.v10_15),
+    ],
     products: [
         .library(
             name: "Server",
