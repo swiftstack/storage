3eabfa37312b267975345629c8b72c4758e41c6c
Tony Freeman <tonyfreeman@users.noreply.github.com>
2024-01-20 10:16:56 +0300

    [Package] Bump darwin platforms

--- Package.swift
+++ Package.swift
@@ -4,8 +4,8 @@ import PackageDescription
 let package = Package(
     name: "Storage",
     platforms: [
-        .iOS(.v13),
-        .macOS(.v10_15),
+        .iOS(.v16),
+        .macOS(.v13),
     ],
     products: [
         .library(
